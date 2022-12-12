064c7894516db14b094fc605d6b4db65a7e74ffe
KOwen26 <58740722+KOwen26@users.noreply.github.com>
2022-12-12 18:35:34 +0700

    First Iteration

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,71 @@
+<div>
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-light p-0">
+        <div class="row gx-0 d-none d-lg-flex">
+            <div class="col-lg-7 px-5 text-start">
+                <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+                    <small class="fa fa-map-marker-alt text-primary me-2"></small>
+                    <small>Kompleks Pergudangan Bizpark C-3, <br> Tambak Sawah, Waru - Sidoarjo </small>
+                </div>
+                <div class="h-100 d-inline-flex align-items-center py-3">
+                    <small class="far fa-clock text-primary me-2"></small>
+                    <small>Mon - Fri : 07.30 AM - 03.30 PM<br>
+                        Sat : 07:30 AM - 02.30 PM</small>
+                </div>
+            </div>
+            <div class="col-lg-5 px-5 text-end">
+                <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+                    <small class="fa fa-phone-alt text-primary me-2"></small>
+                    <small>031-8667469 <br>
+                        031-8677468</small>
+                </div>
+                <div class="h-100 d-inline-flex align-items-center">
+                    <a class="btn btn-sm-square bg-white text-primary me-1" href=""><i
+                            class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-sm-square bg-white text-primary me-1" href=""><i
+                            class="fab fa-twitter"></i></a>
+                    <a class="btn btn-sm-square bg-white text-primary me-1" href=""><i
+                            class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-sm-square bg-white text-primary me-0" href=""><i
+                            class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <h2 class="m-0 text-primary">CV Pelangi UV</h2>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About Us</a>
+                <a href="service.html" class="nav-item nav-link">Our Journey </a>
+                <a href="project.html" class="nav-item nav-link">Our Services</a>
+                <a href="project.html" class="nav-item nav-link">Our Partner</a>
+                <a href="project.html" class="nav-item nav-link">Gallery</a>
+                {{-- <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu fade-up m-0">
+                        <a href="feature.html" class="dropdown-item">Feature</a>
+                        <a href="quote.html" class="dropdown-item">Free Quote</a>
+                        <a href="team.html" class="dropdown-item">Our Team</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                        <a href="404.html" class="dropdown-item">404 Page</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact</a> --}}
+            </div>
+            <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Pesan Sekarang<i
+                    class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+</div>
---
+++ resources/views/components/timeline.blade.php
@@ -0,0 +1,83 @@
+<div>
+    <!-- Experience Start -->
+    <div class="experience" id="experience">
+        <div class="container">
+            <header class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
+                {{-- <p>My Resume</p> --}}
+                <h2>Perjalanan Kami</h2>
+            </header>
+            <div class="timeline">
+                <div class="timeline-item left wow slideInLeft" data-wow-delay="0.1s">
+                    <div class="timeline-text">
+                        <div class="timeline-date">2004</div>
+                        <h2>Awal Cerita Kami</h2>
+                        <h4>Berkat 3 Orang Pejuang</h4>
+                        <p>
+                            Dedikasi dan komitmen tinggi oleh 3 karyawan dengan alat-alat yang serba manual dan didukung
+                            oleh 1 armada pengiriman.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline-item right wow slideInRight" data-wow-delay="0.1s">
+                    <div class="timeline-text">
+                        <div class="timeline-date">2006 - 2008</div>
+                        <h2>Pemindahan Gudang</h2>
+                        <h4>Mulainya Perjalanan yang Lebih Besar</h4>
+                        <p>
+                            Pelangi memindahkan gudangnya ke daerah Tambak Rejo Indah 18. Ada kenaikan pesanan dari
+                            hanya ratusan menjadi ribuan per bulan.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline-item left wow slideInLeft" data-wow-delay="0.1s">
+                    <div class="timeline-text">
+                        <div class="timeline-date">2009 - 2010</div>
+                        <h2>Berkembang Pesat</h2>
+                        <h4>Penambahan Karyawan, Mesin, & Kendaraan</h4>
+                        <p>
+                            Penambahan tim menjadi 15 orang yang didalamnya terdapat 2 perempuan sebagai bagian
+                            administrasi. Didukung dengan adanya 3 mesin spot manual, dan 1 mesin laminating, juga
+                            armada pengiriman yang bertambah 2 dorkas.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline-item right wow slideInRight" data-wow-delay="0.1s">
+                    <div class="timeline-text">
+                        <div class="timeline-date">2011 - 2015</div>
+                        <h2>Mesin Mulai Semi-Otomatis</h2>
+                        <h4>Peningkatan Mesin dan Kendaraan</h4>
+                        <p>
+                            Pesanan perhari naik hingga ribuan. Transaksi terus bertambah pesat. Dengan penambahan mesin
+                            semi-otomatis 2 spot UV dan 1 laminating, 1 mesin UV varnis. Armada pengiriman yang terus
+                            ditambah dengan 1 truk engkel dan 1 pickup. Selain itu Pelangi UV juga mengadakan rekreasi
+                            rutin setiap tahun untuk para anggota tim.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline-item left wow slideInLeft" data-wow-delay="0.1s">
+                    <div class="timeline-text">
+                        <div class="timeline-date">2045 - 2050</div>
+                        <h2>Web Developer</h2>
+                        <h4>Soft Agency, San Francisco, CA</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit
+                            semper pretium.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline-item right wow slideInRight" data-wow-delay="0.1s">
+                    <div class="timeline-text">
+                        <div class="timeline-date">2045 - 2050</div>
+                        <h2>Web Developer</h2>
+                        <h4>Soft Agency, San Francisco, CA</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit
+                            semper pretium.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Job Experience End -->
+</div>
